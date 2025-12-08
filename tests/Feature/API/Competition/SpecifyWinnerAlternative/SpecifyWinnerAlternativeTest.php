b7a1a94a229fdbebe04780aa983756df9ef1511b
Dean Haines <vbpupil@gmail.com>
2025-12-08 12:58:10 +0000

    tests fixed

--- tests/Feature/API/Competition/SpecifyWinnerAlternative/SpecifyWinnerAlternativeTest.php
+++ tests/Feature/API/Competition/SpecifyWinnerAlternative/SpecifyWinnerAlternativeTest.php
@@ -21,23 +21,15 @@ class SpecifyWinnerAlternativeTest extends TestCase
 
         $this->login();
 
-        $this->competitionA = Competition::factory([
-            'type' => 'DAILY',
-            'start' => '2024-10-01 09:00:00',
-            'end' => '2024-10-05 17:00:00',
-            'active_from' => '15:10',
-            'active_to' => '15:00',
-        ])->hasPhoneLines(1, ['phone_number' => '03000111111'])->create();
+        list($organisation, $phoneBookEntry, $this->competition, $this->phoneLine, $competitionNumber, $callerNumber) = $this->setCompetition();
 
-        Participant::factory(['competition_id' => $this->competitionA->id, 'competition_phone_number'=> '03000111111','competition_phone_line_id' => $this->competitionA->phoneLines()->first()->id, 'call_start' => '2024-10-02 09:00:00','call_end' => '2024-10-02 09:00:57', 'call_id'=>'555'])->count(10)->create();
-        Participant::factory(['competition_id' => $this->competitionA->id, 'competition_phone_number'=> '03000111112','competition_phone_line_id' => $this->competitionA->phoneLines()->first()->id, 'call_start' => '2024-10-09 09:00:00','call_end' => '2024-10-02 09:00:55'])->count(10)->create();
-        Participant::factory(['competition_id' => $this->competitionA->id, 'competition_phone_number'=> '03000111113','competition_phone_line_id' => $this->competitionA->phoneLines()->first()->id, 'call_start' => '2024-10-16 09:00:00','call_end' => '2024-10-02 09:00:52'])->count(10)->create();
-        Participant::factory(['competition_id' => $this->competitionA->id, 'competition_phone_number'=> '03000111114','competition_phone_line_id' => $this->competitionA->phoneLines()->first()->id, 'call_start' => '2024-10-22 09:00:00','call_end' => '2024-10-02 09:00:50'])->count(10)->create();
+
+        Participant::factory(['competition_id' => $this->competition->id, 'competition_phone_number'=> $this->phoneLine->phone_number, 'call_start' => '2024-10-02 09:00:00','call_end' => '2024-10-02 09:00:57', 'call_id'=>555])->count(10)->create();
     }
 
     public function test_validation()
     {
-        $this->post(route('competition.specify-winner.alternative',$this->competitionA) )
+        $this->post(route('competition.specify-winner.alternative',$this->competition) )
             ->assertUnprocessable()
             ->assertJson(function (AssertableJson $json) {
                 return $json
@@ -50,7 +42,7 @@ class SpecifyWinnerAlternativeTest extends TestCase
 
     public function test_out_of_bounds_exception()
     {
-        $this->post(route('competition.specify-winner.alternative',$this->competitionA), [
+        $this->post(route('competition.specify-winner.alternative',$this->competition), [
             'position' => 999,
             'date_from' => '2024-10-01T09:00:00Z',
             'date_to' => '2024-10-05T17:00:00Z',
@@ -61,7 +53,7 @@ class SpecifyWinnerAlternativeTest extends TestCase
     {
         $this->assertCount(0, CompetitionWinnerAlt::all());
 
-        $this->post(route('competition.specify-winner.alternative',$this->competitionA), [
+        $this->post(route('competition.specify-winner.alternative',$this->competition), [
             'position' => 4,
             'date_from' => '2024-10-01T09:00:00Z',
             'date_to' => '2024-10-05T17:00:00Z',
@@ -72,12 +64,11 @@ class SpecifyWinnerAlternativeTest extends TestCase
                     ->has('data.id')
                     ->has('data.attributes.participant_id')
                     ->has('data.attributes.competition_id')
-                    ->where('data.attributes.call_id', '555')
+                    ->where('data.attributes.call_id', 555)
                     ->where('data.attributes.number_of_entries', 1)
                     ->where('data.attributes.date_from', '2024-10-01 09:00:00')
                     ->where('data.attributes.date_to', '2024-10-05 17:00:00')
-                    ->where('data.attributes.phone_line_id', $this->competitionA->phoneLines()->first()->id)
-                    ->where('data.attributes.competition_phone_number', '03000111111')
+                    ->where('data.attributes.competition_phone_number', $this->phoneLine->phone_number)
                     ->where('data.attributes.call_start', '2024-10-02 09:00:00')
                     ->where('data.attributes.call_end', '2024-10-02 09:00:57')
                     ->has('data.attributes.telephone')
@@ -88,16 +79,15 @@ class SpecifyWinnerAlternativeTest extends TestCase
         $this->assertCount(1, $winners = CompetitionWinnerAlt::all());
 
         tap($winners->first(), function(CompetitionWinnerAlt $winner){
-            $participant = Participant::where('competition_id', $this->competitionA->id)->skip(4 - 1)->take(1)->first();
+            $participant = Participant::where('competition_id', $this->competition->id)->skip(4 - 1)->take(1)->first();
 
             $this->assertSame($participant->id, $winner->participant_id);
             $this->assertSame($participant->competition_id, $winner->competition_id);
-            $this->assertSame('555', $winner->call_id);
+            $this->assertEquals('555', $winner->call_id);
             $this->assertEquals(1, $winner->number_of_entries);
             $this->assertSame('2024-10-01 09:00:00', $winner->date_from);
             $this->assertSame('2024-10-05 17:00:00', $winner->date_to);
-            $this->assertSame($participant->competition_phone_line_id, $winner->phone_line_id);
-            $this->assertSame('03000111111', $winner->competition_phone_number);
+            $this->assertSame($participant->competition_phone_number, $winner->competition_phone_number);
             $this->assertSame($participant->telephone, $winner->telephone);
             $this->assertSame('2024-10-02 09:00:00', $winner->call_start);
         });
